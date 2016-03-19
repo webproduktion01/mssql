93060e68b7ff2baef6e588bc0da4c6b6c0ab2e4c
a b <anders9943@hotmail.com>
2016-03-19 13:08:52 +0100

    Did all the queries. Might need to add some more data to really show the queries work as intended. Might not be necessary tho

---
+++ CrewManifest/Requested queries/DeathNameCauseAndDate.sql
@@ -0,0 +1,4 @@
+﻿select c.FirstName +' '+c.LastName as 'Name', dc.DeathCause as 'Cause of Death',d.DeathDate as 'Date'
+from CrewMember as c
+inner join Death as d on d.DeathID = c.DeceasedID
+inner join DeathCause dc on d.DeathCauseID = dc.DeathCausedID;
