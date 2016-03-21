333468e412a28151eb4b32a9a288b99a4cd8b961
oracleliaprojekt@gmail.com <oracleliaprojekt@gmail.com>
2016-03-21 09:11:45 +0100

    Made a simple stored procedure based on the between dates requested query specification

---
+++ Nostromo_Database/StoredProcedures/NosBetweenDates.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[NosBetweenDates]
+	@dateone datetime,
+	@datetwo datetime
+AS
+	select c.FirstName, d.DeathDate
+	from CrewMember as c
+	inner join Death as d on c.DeceasedID=d.DeathID
+	where d.DeathDate between @dateone and @datetwo;
+RETURN 0
